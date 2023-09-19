6517856b2f02e2b9927f98a4d5d54416dbe69b10
shlyzhas <8willox8@gmail.com>
2023-09-19 23:59:45 +0300

    optimization 5 task

--- 5/5.py
+++ 5/5.py
@@ -1,30 +1,42 @@
-def closeness(arr1, arr2, rgr):
-    while rgr >= 0 and arr1[rgr] != arr2[rgr]:
-        rgr -= 1
-    return rgr + 1
+class Tree:
+    class Node:
+        def __init__(self):
+            self.count = 1
+            self.children = {}
 
-def exp_search(arr1, arr2) -> int:
-    if arr1[0] != arr2[0]:
-        return 0
+    def __init__(self):
+        self.sum = 0
+        self.root = self.Node()
 
-    min_size = min(len(arr1), len(arr2))
-    rgr = 1
-    while (rgr < min_size and arr1[rgr] == arr2[rgr]):
-        rgr = rgr << 1
-    if rgr > min_size - 1:
-        rgr = min_size - 1
+    def add(self, array):
+        current_node = self.root
+        for el in array:
+            if el in current_node.children:
+                self.sum += current_node.children[el].count
+                current_node.children[el].count += 1
+            else:
+                current_node.children[el] = self.Node()
+            current_node = current_node.children[el]
 
-    return closeness(arr1, arr2, (rgr + 1) >> 1)
-    
-array_count = int(input())
-array_of_arrays = []
-for _ in range(array_count):
-    _ = input()
-    array_of_arrays.append(list(map(int, input().split(' '))))
-    
-sum = 0
+    def print(self):
+        self._dfs(self.root)
 
-for i in range(len(array_of_arrays)):
-    for j in range(i + 1, len(array_of_arrays)):
-        sum += exp_search(array_of_arrays[i], array_of_arrays[j])
-print(sum)
+    def _dfs(self, node):
+        for el in node.children:
+            print(f"{el}")
+            self._dfs(node.children[el])
+
+
+tree = Tree()
+# tree.add([1,2,3,4])
+# tree.add([1,2,3,5])
+# tree.add([3,3,3])
+# tree.print()
+number_of_arrays = int(input())
+
+for _ in range(number_of_arrays):
+    input()
+    tree.add(input().split())
+
+# tree.print()
+print(tree.sum)
